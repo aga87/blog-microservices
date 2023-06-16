1f834a8afbb7c96bb88b11d147a867f596a876ef
aga87 <aga.labonarska@outlook.com>
2023-06-16 14:19:54 +0200

    Start the server and listen for connections

---
+++ posts/src/index.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import express, { Application, Request, Response } from 'express';
+
+dotenv.config();
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+const { PORT } = process.env;
+const port = PORT || 4000;
+app.listen(PORT, () => console.log(`Listening on port ${port}...`));
