7515c13f97475182133f69c17219af5438e7e59f
aga87 <aga.labonarska@outlook.com>
2023-06-20 17:13:20 +0200

    Set up the data query service

---
+++ query/src/index.ts
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+dotenv.config();
+const app: Application = express();
+
+// Parse application/json
+app.use(express.json());
+
+// Set CORS
+const { CLIENT_URL } = process.env;
+const host = CLIENT_URL || 'http://localhost:3000';
+app.use(
+  cors({
+    origin: host
+  })
+);
+
+// Test route
+app.get('/ping', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+// Listen for connections
+const { PORT } = process.env;
+const port = PORT || 4002;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
