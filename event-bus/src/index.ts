c0f8d00ef8c8b042d7af298177b643215c66d802
aga87 <aga.labonarska@outlook.com>
2023-06-20 15:58:13 +0200

    Implement a basic event bus

--- event-bus/src/index.ts
+++ event-bus/src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import axios from 'axios';
 
 dotenv.config();
 const app: Application = express();
@@ -17,6 +18,33 @@ app.use(
 
 app.get('/ping', (req: Request, res: Response) => {
   res.send('Hello World');
+});
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  const { POSTS_SERVICE_URL, COMMENTS_SERVICE_URL, QUERY_SERVICE_URL } =
+    process.env;
+
+  axios
+    .post(POSTS_SERVICE_URL || 'http://localhost:4000', event)
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+  axios
+    .post(COMMENTS_SERVICE_URL || 'http://localhost:4001/events', event)
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+  axios
+    .post(QUERY_SERVICE_URL || 'http://localhost:4002/events', event)
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+  return res.send({ status: 'OK' });
 });
 
 const { PORT } = process.env;
