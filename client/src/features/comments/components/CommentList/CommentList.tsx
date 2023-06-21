f3bab0e984565cd00fe0e5f96c394e6bc888553a
aga87 <aga.labonarska@outlook.com>
2023-06-21 12:23:58 +0200

    Use the query service to minimise the no of request & increase reliability

--- client/src/features/comments/components/CommentList/CommentList.tsx
+++ client/src/features/comments/components/CommentList/CommentList.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import { Error, Loader } from '../../../../components';
-import { useDelayedLoader } from '../../../../hooks';
 import type { Comment } from '../../types/comment.types';
 
 type CommentListProps = {
   postComments: Comment[];
-  isLoading: boolean;
-  serverError: string | null;
 };
 
-export const CommentList = ({
-  postComments,
-  isLoading,
-  serverError
-}: CommentListProps) => {
-  const { isLoaderVisible } = useDelayedLoader({ isLoading, delay: 1000 });
-
+export const CommentList = ({ postComments }: CommentListProps) => {
   const commentListItems = postComments.map(comment => {
     return (
       <li key={comment.id} className='py-2 border-b border-slate-200'>
@@ -24,7 +14,5 @@ export const CommentList = ({
     );
   });
 
-  if (isLoaderVisible) return <Loader />;
-  if (serverError) return <Error text={serverError} />;
   return <ul>{commentListItems}</ul>;
 };
--- client/src/features/posts/services/PostsApiService.ts
+++ client/src/features/posts/services/PostsApiService.ts
@@ -1,8 +1,13 @@
 import { createApiInstance, getError } from '../../../libs/axios';
 import type { NewPost, Post } from '../types/posts.types';
 
-const getApiInstance = () => {
-  const baseURL = process.env.POSTS_API_BASE_URL || 'http://localhost:4000';
+const getApiInstance = (service: 'posts' | 'query') => {
+  let baseURL;
+  if (service === 'posts') {
+    baseURL = process.env.POSTS_API_BASE_URL || 'http://localhost:4000';
+  } else {
+    baseURL = process.env.QUERY_API_BASE_URL || 'http://localhost:4002';
+  }
   const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json'
@@ -12,7 +17,7 @@ const getApiInstance = () => {
 
 export const fetchPosts = async () => {
   try {
-    const res = await getApiInstance().get('/posts');
+    const res = await getApiInstance('query').get('/posts');
     const posts: Post[] = res.data;
     return { posts, error: null };
   } catch (err: unknown) {
@@ -22,7 +27,7 @@ export const fetchPosts = async () => {
 
 export const createPost = async (newPost: NewPost) => {
   try {
-    const res = await getApiInstance().post('/posts', newPost);
+    const res = await getApiInstance('posts').post('/posts', newPost);
     const post: Post = res.data;
     return { post, error: null };
   } catch (err: unknown) {
