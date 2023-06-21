4096e778164832bffc502152a542e0b6f09a241c
aga87 <aga.labonarska@outlook.com>
2023-06-21 15:44:36 +0200

    Update the UI - render comment status

--- client/src/features/comments/types/comment.types.ts
+++ client/src/features/comments/types/comment.types.ts
@@ -1,6 +1,7 @@
 export type Comment = {
   id: string;
   content: string;
+  status: 'pending' | 'approved' | 'rejected';
 };
 
-export type NewComment = Omit<Comment, 'id'>;
+export type NewComment = Omit<Comment, 'id' | 'status'>;
